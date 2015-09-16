805f0406d06cb4cb3088d0c18ca292eba3fcb22c
Jan Kosinski <jan.kosinski@embl.de>
2015-09-16 17:19:53 +0200

    get_distances.py: refactor

--- scripts/get_distances.py
+++ scripts/get_distances.py
@@ -8,56 +8,76 @@ from optparse import OptionParser
 import xlinkanalyzer as xla
 import xlinkanalyzer.data
 import xlinkanalyzer.manager
+import xlinkanalyzer.data
 import chimera
 
+def getXlinkItems(config):
+    data = []
+    for item in config.dataItems:
+        if xlinkanalyzer.data.isXlinkItem(item):
+            if item.data.active:
+                data.append(item.data)
 
-usage = "usage: %prog [options] xquest_filename dist > some_name.csv"
-parser = OptionParser(usage=usage)
+    return data
 
-parser.add_option("--proj", dest="proj_fn", default=None,
-                  help="path to project json file [default: %default]")
+def loadFromStructure(chimeraModel, xfiles):
+    config = xlinkanalyzer.data.Assembly()
+    config.loadFromStructure(chimeraModel)
 
-(options, args) = parser.parse_args()
+    fg = xla.data.FileGroup(files=xfiles)
+    xi = xla.data.XQuestItem(fileGroup=fg, config=config)
+    config.addItem(xi)
+    for name in xi.getMappingElements()[0]:
+        guess = xi.getMappingDefaults(name)
+        if guess is not None:
+            xi.mapping[name] = [guess.name]
+        # else:
+        #     xi.mapping[name] = None
 
-model_filename = args[0]
-xfiles = args[1:]
+    print 'Mapping: ', xi.mapping
 
-print model_filename
+    return config
 
-chimera.openModels.open(model_filename)
-chimeraModel = chimera.openModels.list()[-1]
+def main():
+    usage = "usage: %prog [options] xquest_filename dist > some_name.csv"
+    parser = OptionParser(usage=usage)
 
-config = xlinkanalyzer.data.Assembly()
-config.loadFromStructure(chimeraModel)
+    parser.add_option("--proj", dest="proj_fn", default=None,
+                      help="path to project json file [default: %default]")
 
-fg = xla.data.FileGroup(files=xfiles)
-xi = xla.data.XQuestItem(fileGroup=fg, config=config)
+    (options, args) = parser.parse_args()
 
-for name in xi.getMappingElements()[0]:
-    guess = xi.getMappingDefaults(name)
-    if guess is not None:
-        xi.mapping[name] = [guess.name]
-    # else:
-    #     xi.mapping[name] = None
+    model_filename = args[0]
+    xfiles = args[1:]
 
-print 'Mapping: ', xi.mapping
+    print model_filename
 
-model = xla.manager.Model(chimeraModel, config)
-mgr = xla.manager.XlinkDataMgr(model, [xi])
-mgr.showAllXlinks()
+    chimera.openModels.open(model_filename)
+    chimeraModel = chimera.openModels.list()[-1]
 
-wrong = mgr.getNonCrosslinkableXlinks()
-if len(wrong) > 0:
-    print 'Non crosslinkable resi xlinked'
-    for xl in wrong:
-        print ', '.join([xl.xlink.get('Protein1'), xl.xlink.get('Protein2'), xl.xlink.get('AbsPos1'), xl.xlink.get('AbsPos2'), xl.xlink.get('Spectrum'), xl.xlink.get('Type'), xl.xlink.get('XLType'), xl.xlink.get('ld-Score')])
+    if options.proj_fn:
+        pass
+    else:
+        config = loadFromStructure(chimeraModel, xfiles)
+
+    model = xla.manager.Model(chimeraModel, config)
+    mgr = xla.manager.XlinkDataMgr(model, getXlinkItems(config))
+    mgr.showAllXlinks()
+
+    wrong = mgr.getNonCrosslinkableXlinks()
+    if len(wrong) > 0:
+        print 'Non crosslinkable resi xlinked'
+        for xl in wrong:
+            print ', '.join([xl.xlink.get('Protein1'), xl.xlink.get('Protein2'), xl.xlink.get('AbsPos1'), xl.xlink.get('AbsPos2'), xl.xlink.get('Spectrum'), xl.xlink.get('Type'), xl.xlink.get('XLType'), xl.xlink.get('ld-Score')])
 
 
-stats = mgr.countSatisfied(xlinkanalyzer.XLINK_LEN_THRESHOLD)
+    stats = mgr.countSatisfied(xlinkanalyzer.XLINK_LEN_THRESHOLD)
 
-print 'All mapped: {0}'.format(stats['all'])
+    print 'All mapped: {0}'.format(stats['all'])
 
-filename = os.path.splitext(os.path.basename(model_filename))[0] + '.csv'
-mgr.exportXlinksWithDistancesToCSV(stats, filename)
-print filename
+    filename = os.path.splitext(os.path.basename(model_filename))[0] + '.csv'
+    mgr.exportXlinksWithDistancesToCSV(stats, filename)
+    print filename
 
+if __name__ == '__main__':
+    main()
