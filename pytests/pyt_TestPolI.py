7fb3e99a2ac93180f6f34bbeea2c27c78e1eebda
Jan Kosinski <jan.kosinski@embl.de>
2016-01-18 18:08:05 +0100

    Extending pyt_TestPolI test

--- pytests/pyt_TestPolI.py
+++ pytests/pyt_TestPolI.py
@@ -19,4 +19,24 @@ class TestPolI(XlaGuiTests.XlaBaseTest):
         super(TestPolI, self).setUp(mPaths, cPath)
 
     def testPolI(self):
-        pass
+        ms = xla.get_gui().Subunits.table.modelSelect
+        m = chimera.openModels.list()[0]
+        ms.setvalue([m])
+
+        g = xla.get_gui()
+
+        g.modelSelect.doSync(ms)
+
+        xFrame = g.Xlinks
+
+        xFrame.displayDefault()
+
+        self.assertEqual(1, len(xFrame.getXlinkDataMgrs()))
+
+        xmgr = xFrame.getXlinkDataMgrs()[0]
+        self.assertEqual(171, len(xmgr.pbg.pseudoBonds))
+
+        xFrame.ld_score_var.set(30.0)
+
+        displayed = len([pb for pb in xmgr.pbg.pseudoBonds if pb.display == True])
+        self.assertEqual(106, displayed)
