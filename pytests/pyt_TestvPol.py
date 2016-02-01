37f6b8263de6cf9a93b08c6f2e5d3ba0f48ea533
Jan Kosinski <jan.kosinski@embl.de>
2016-02-01 15:10:56 +0100

    New tests

---
+++ pytests/pyt_TestvPol.py
@@ -0,0 +1,46 @@
+import chimera
+
+import XlaGuiTests
+
+import xlinkanalyzer as xla
+
+# for this test to run do:
+# chimera --pypath ~/devel/XlinkAnalyzer --pypath ~/devel/pyxlinks/ run.py <name of this file>
+RUNME = True
+
+description = "Tests gui class"
+
+
+class TestPolI(XlaGuiTests.XlaBaseTest):
+
+    def setUp(self):
+        mPaths = ['vPol/5FMZ.pdb']
+        cPath = 'vPol/vPol.json'
+        super(TestPolI, self).setUp(mPaths, cPath)
+
+    def testPolI(self):
+        ms = xla.get_gui().Subunits.table.modelSelect
+        m = chimera.openModels.list()[0]
+        ms.setvalue([m])
+
+        g = xla.get_gui()
+
+        g.modelSelect.doSync(ms)
+
+        xFrame = g.Xlinks
+
+        xFrame.displayDefault()
+
+        self.assertEqual(1, len(xFrame.getXlinkDataMgrs()))
+
+        xmgr = xFrame.getXlinkDataMgrs()[0]
+        # self.assertEqual(171, len(xmgr.pbg.pseudoBonds))
+
+        xFrame.ld_score_var.set(30.0)
+
+        displayed = len([pb for pb in xmgr.pbg.pseudoBonds if pb.display == True])
+        # self.assertEqual(106, displayed)
+
+        #just test open windows
+        # g.configFrame.domainsButton.invoke()
+        # g.configFrame.subCompButton.invoke()
