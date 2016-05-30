c6af078acf6e2969791442b78ed1716a51191c08
Jan Kosinski <jan.kosinski@embl.de>
2016-05-30 15:37:03 +0200

    Update version number to 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='XlinkAnalyzer',
-    version='0.99dev',
+    version='1.1.1',
     author='Jan Kosinski',
     author_email='jan.kosinski@embl.de',
     packages=['xlinkanalyzer'
